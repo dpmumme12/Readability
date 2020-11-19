ffa28b3584af5d656d5fc245820f5eaa4ce50ca2
Doug Mumme <dougmumme@gmail.com>
2020-11-18 20:56:13 -0600

    fixed string input

--- readability.c
+++ readability.c
@@ -2,26 +2,48 @@
 #include <ctype.h>
 #include <string.h>
 #include <math.h>
+#include <stdlib.h>
+
+char *inputString(FILE* fp, size_t size){
+//The size is extended by the input with the value of the provisional
+    char *str;
+    int ch;
+    size_t len = 0;
+    str = realloc(NULL, sizeof(char)*size);//size is start size
+    if(!str)return str;
+    while(EOF!=(ch=fgetc(fp)) && ch != '\n'){
+        str[len++]=ch;
+        if(len==size){
+            str = realloc(str, sizeof(char)*(size+=16));
+            if(!str)return str;
+        }
+    }
+    str[len++]='\0';
+
+    return realloc(str, sizeof(char)*len);
+}
 
 int main(void)
 {
-    printf("text: ");
-    char *text; 
-    scanf("%s", text); 
+    char *text;
+    printf("text: "); 
+    text =  inputString(stdin, 10);
+
+    
 
 
     float letters = 0, words = 1, sentences = 0;
-    for (int x = 0, c = strlen(&text); x < c; x++) // loop to get number of letters, words and sentences
+    for (int x = 0, c = strlen(text); x < c; x++) // loop to get number of letters, words and sentences
     {
-        if (isalpha(&text[x])) // getting number of letters
+        if (isalpha(text[x])) // getting number of letters
         {
             letters++;
         }
-        if (isblank(&text[x])) // getting number of words
+        if (isblank(text[x])) // getting number of words
         {
             words++;
         }
-        if (&text[x] == '.' || &text[x] == '!' || &text[x] == '?') // getting number of sentences
+        if (text[x] == '.' || text[x] == '!' || text[x] == '?') // getting number of sentences
         {
             sentences++;
         }
@@ -42,4 +64,7 @@ int main(void)
     {
         printf("Grade 16+\n");
     }
+
+    free(text);
+    return 0;
 }
